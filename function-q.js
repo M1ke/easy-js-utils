684636a657d6ac9b24e5e698af58ff8f9920b16c
m1ke <git@m1ke.me>
2015-05-12 10:36:20 +0100

    Added new function q

--- function-q.js
+++ function-q.js
@@ -1,5 +1,23 @@
-var functionQ = function(fn, context, params){
-	return function(){
-		fn.apply(context, params);
-	};
-}
+var functionQ = function(){
+	var _queue = [];
+
+	function _add(fn){
+		_queue.push(fn);
+	}
+
+	function _wrap(fn, context, params){
+		_add(function(){
+			fn.apply(context, params);
+		});
+	}
+
+	function _run(){
+		while (_queue.length>0){
+			(_queue.shift())();
+		}
+	}
+
+	this.add = _add;
+	this.run = _run;
+	this.wrap = _wrap;
+};
