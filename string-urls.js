3e7b85625db96977833cc135314a68553a0334ea
m1ke <git@m1ke.me>
2015-08-28 15:00:54 +0100

    String urls

---
+++ string-urls.js
@@ -0,0 +1,14 @@
+function findUrls(text){
+    var source = (text || '').toString()
+    	,urlArray = []
+    	,matchArray
+    	,regexToken = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?,&\/\/=]+)|((mailto:)?[_.\w-]+@([\w][\w\-]+\.)+[a-zA-Z]{2,3})/g;
+
+    // Iterate through any URLs in the text.
+    while ((matchArray = regexToken.exec(source))!==null){
+        var token = matchArray[0];
+        urlArray.push(token);
+    }
+
+    return urlArray;
+}
