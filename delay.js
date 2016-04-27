86ac23b210f67a5ea5e38dd5546a52d72408d38a
m1ke <git@m1ke.me>
2016-04-27 11:38:50 +0100

    Altered delay to allow creation of individual delays

--- delay.js
+++ delay.js
@@ -1,7 +1,9 @@
-var delay = (function(){
+var delayBase = function(){
 	var timer = 0;
 	return function(callback, ms){
 		clearTimeout(timer);
 		timer = setTimeout(callback, ms);
 	};
-})();
+};
+
+var delay = delayBase();
